d837e9a22b3a5d9056498446e025f8937de1161e
Francesco Pandolfi <francesco.pan@gmail.com>
2021-12-08 16:18:04 +0100

    update be

--- src/config/socket.ts
+++ src/config/socket.ts
@@ -2,7 +2,7 @@ import io from "socket.io-client";
 
 const ENDPOINT =
   process.env.NODE_ENV === 'production'
-    ? 'https://lets--draw.herokuapp.com/'
+    ? 'https://drawo-be.herokuapp.com/'
     : 'http://localhost:3001/';
 
 const socket = io(ENDPOINT);
